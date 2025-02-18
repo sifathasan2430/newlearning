145437f955575a0d2382e334b4d91641da0c4939
sifathasan2430 <abdussalam243028@gmail.com>
2025-02-18 08:48:07 +0000

    first section done

---
+++ newfolder1/practice01.js
@@ -0,0 +1,12 @@
+function height(objects){
+    let weight=objects.height;
+    let height=objects.weight
+    Bmicalculatvalue=weight*height
+return `BMI is ${Bmicalculatvalue} `
+}
+let object={
+    name :'sifat',
+    weight :88,
+    height: 5.8
+}
+console.log(height(object));
